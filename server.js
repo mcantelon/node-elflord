bacd6f5db4992ba7c35fb99101fc683eaba90884
Mike Cantelon <mcantelon@gmail.com>
2011-12-27 16:39:12 -0800

    Used module.exports to pass db instead of manually requiring db in each controller module.

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var express = require('express')
   , er = require('express-resource')
-  , app = express.createServer();
+  , app = express.createServer()
+  , dirty = require('dirty')
+  , db = require('dirty')('t.db');
 
-app.use(express.bodyParser());
+db.on('load', function() {
+  app.use(express.bodyParser());
 
-app.resource('tasks', require('./lib/controllers/tasks'));
-app.resource('categories', require('./lib/controllers/categories'));
+  app.resource('tasks', require('./lib/controllers/tasks')(db));
+  app.resource('categories', require('./lib/controllers/categories')(db));
 
-app.listen(8000);
+  app.listen(8000);
+});
