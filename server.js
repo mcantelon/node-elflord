a758cada8dd7ab2d3c1d590073a6adcbfed86a9f
Mike Cantelon <mcantelon@gmail.com>
2011-12-28 19:30:11 -0800

    Moved controller directory.

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ var express = require('express')
   , er = require('express-resource')
   , app = express.createServer()
   , dirty = require('dirty')
-  , db = require('dirty')('t.db');
+  , db = require('dirty')('t.db')
+  , controllerDir = './lib/server/controllers';
 
 db.on('load', function() {
   app.use(express.bodyParser());
 
-  app.resource('tasks', require('./lib/controllers/tasks')(db));
-  app.resource('categories', require('./lib/controllers/categories')(db));
+  app.resource('tasks', require(controllerDir + '/tasks')(db));
+  app.resource('categories', require(controllerDir + '/categories')(db));
 
   app.listen(8000);
 });
