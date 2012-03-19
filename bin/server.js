8148a12a13b06c4daa123749fb1176d7d26be4c8
Mike Cantelon <mcantelon@gmail.com>
2012-03-18 17:27:57 -0700

    Made server database file location configurable.

--- bin/server.js
+++ bin/server.js
@@ -3,9 +3,10 @@
 var path = require('path')
   , argv = require('optimist').argv
   , fs = require('fs')
+  , helpers = require('../lib/helpers')
   , server = require('../lib/server/server.js');
 
-var configFile = path.join(process.env.HOME, '.elflord-server')
+var configFile = path.join(helpers.homeDir(), '.elflord-server')
   , config;
 
 // allow overriding of default configuration location
@@ -15,7 +16,12 @@ path.exists(configFile, function(exists) {
   if (exists) {
     fs.readFile(configFile, 'utf8', function(err, data) {
       if (err) throw err;
+
       config = JSON.parse(data.toString());
+
+      // allow overriding of default DB location
+      config.dbFile = argv['d'] || config.db || 'elflord-server.db';
+
       server.run(argv, config);
     });
   } else {
