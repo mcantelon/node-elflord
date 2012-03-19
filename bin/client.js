a558382b362db5938ce107aef81ad2e9af02256a
Mike Cantelon <mcantelon@gmail.com>
2012-03-18 17:45:22 -0700

    Made server database file location configurable in client.

--- bin/client.js
+++ bin/client.js
@@ -15,7 +15,12 @@ path.exists(configFile, function(exists) {
   if (exists) {
     fs.readFile(configFile, 'utf8', function(err, data) {
       if (err) throw err;
+
       config = JSON.parse(data.toString());
+
+      // allow overriding of default DB location
+      config.dbFile = argv['d'] || config.db || 'elflord.db';
+
       client.run(argv, config);
     });
   } else {
